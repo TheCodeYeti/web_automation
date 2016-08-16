a7a129c1875a6ac8d91240ed0b50c5217bd6a7ce
spencer <spencer.dossett@gmail.com>
2016-08-16 13:45:44 -0400

    improved error logging to handle multiple reasons for failing a given test

---
+++ features/step_definitions/check_badge.rb
@@ -0,0 +1,25 @@
+Then(/^the amount on the badge and the price reduction match$/) do
+  error_list = nil
+  @browser.divs(class: "module-grocery-product").each do |item|
+
+    badge_savings_string = item.span(class: "deal-type").text
+    badge_savings = badge_savings_string.tr('SAVE $', '').to_f
+
+    current_price_string = item.span(class: "reg-price").text
+    current_price = current_price_string.tr('$', '').to_f
+
+    old_price_string = item.span(class: "old-price").text
+    old_price = old_price_string.tr('$', '').to_f
+
+    name = item.a(class: "product-name").text
+
+    unless (old_price - current_price).round(2) == badge_savings
+      error_list ||= "Badge test failed for the following reassons:\n"
+      error_list += "#{name} badge savings of #{badge_savings_string} does not match given sale price of #{current_price_string} and an original price of #{old_price_string}\n"
+    end
+
+    # binding.pry
+  end
+raise error_list if error_list
+
+end
--- features/step_definitions/validate_sort.rb
+++ features/step_definitions/validate_sort.rb
@@ -1,4 +1,5 @@
 Then(/^I should see a list of "([^"]*)" sorted by "([^"]*)"$/) do |items, sort_criteria|
+  error_list = nil
   prior_price = 0
   @browser.divs(class: "module-grocery-product").each do |item|
     name = item.a(class: "product-name").text
@@ -6,10 +7,11 @@ Then(/^I should see a list of "([^"]*)" sorted by "([^"]*)"$/) do |items, sort_c
     price = price_string.tr('$','').to_f
     if price < prior_price then
       # binding.pry
-      fail(StandardError.new("sorting test failed on #{name} with a price of #{price}"))
+      error_list ||= "Sorting test failed for the following reasons:\n"
+      error_list += "#{name} with a price of #{price} is out of place\n"
     end
     prior_price = price
-
   end
+  raise StandardError.new(error_list) if error_list
   # module-grocery-product
 end
