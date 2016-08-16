61238d0781b4091206b464b8952c8d4b61bdf785
spencer <spencer.dossett@gmail.com>
2016-08-15 20:00:47 -0400

    added definition to validate sort

--- features/step_definitions/validate_sort.rb
+++ features/step_definitions/validate_sort.rb
@@ -1,8 +1,15 @@
 Then(/^I should see a list of "([^"]*)" sorted by "([^"]*)"$/) do |items, sort_criteria|
+  prior_price = 0
   @browser.divs(class: "module-grocery-product").each do |item|
     name = item.a(class: "product-name").text
-    price = item.span(class: "reg-price").text
-    binding.pry
+    price_string = item.span(class: "reg-price").text
+    price = price_string.tr('$','').to_f
+    if price == prior_price then
+      binding.pry
+      fail(StandardError.new("sorting test failed on #{name} with a price of #{price}"))
+    end
+    prior_price = price
+
   end
   # module-grocery-product
 end
